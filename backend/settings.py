d38ea166bbe2b384abc2f3bd3cb257d989deac38
behmah <b2000yasini@gmail.com>
2024-12-29 15:13:24 +0430

    get rid of some changes

--- backend/settings.py
+++ backend/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),  # Expiration time
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
